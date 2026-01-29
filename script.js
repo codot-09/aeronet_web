6ee2d5f64dd355c4244aad54f2827d88463c941e
codot-09 <onabiyev626@gmail.com>
2026-01-29 22:28:07 +0500

    nimadir

--- script.js
+++ script.js
@@ -47,24 +47,4 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Fullscreen Function
-function toggleFullScreen() {
-    var elem = document.getElementById("presentationBlock");
 
-    if (!document.fullscreenElement) {
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) { /* Safari */
-            elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) { /* IE11 */
-            elem.msRequestFullscreen();
-        }
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) { /* Safari */
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { /* IE11 */
-            document.msExitFullscreen();
-        }
-    }
-}
